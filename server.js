3fc9afc63f33290fe4e587a9b50a5f9035764ae1
Yogesh <yogeshvamci707@gmail.com>
2026-01-19 20:41:13 +0530

    Add Docker setup and README updates

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('ZKP Docker container running successfully!');
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
